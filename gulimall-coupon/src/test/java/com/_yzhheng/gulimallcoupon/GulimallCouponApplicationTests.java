63a8bd7edbc413acc6554b83c0fe5fb3add836bd
Kenny628 <kennyyong628@gmail.com>
2024-04-30 20:42:15 +0800

    first commit

---
+++ gulimall-coupon/src/test/java/com/_yzhheng/gulimallcoupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com._yzhheng.gulimallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
